4b58e7e04fa3f5f2ea0c19115535a589b2a1eff4
Denys Shevchenko <denys.shevchenko.pro@gmail.com>
2025-10-19 22:05:21 +0200

    update heroSection, aboutSection

--- src/widgets/About/About.jsx
+++ src/widgets/About/About.jsx
@@ -1,13 +1,17 @@
 import { useState } from 'react';
-import clsx from 'clsx';
 import { accordionData } from '@/shared/data/accordionData';
 import { Button } from '@/shared/ui/Button';
-import { AccordionButton } from '@/shared/ui/AccordionButton';
+import clsx from 'clsx';
 import aboutImgSrc from '@/assets/img/about/about-img.svg';
+import plusIcon from '@/assets/img/about/plus.png';
 import styles from './About.module.scss';
 
 export const About = () => {
   const [activeAccordion, setActiveAccordion] = useState(0);
+
+  const handleAccordionClick = (index) => {
+    setActiveAccordion(activeAccordion === index ? null : index);
+  };
 
   return (
     <section id="about" className={styles.aboutSection}>
@@ -19,25 +23,34 @@ export const About = () => {
           <div className={styles.aboutContent}>
             <div className={styles.sectionTitle}>
               <h1>Read more about our Digital Agency</h1>
-              <p>
+              <p className="description">
                 Lorem ipsum dolor sit amet, consetetur sadipscing elitr,sed diam nonumy eirmod tempor invidunt ut labore
                 et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores.
               </p>
             </div>
             <div className={styles.accordion}>
-              {accordionData.map((item, index) => (
-                <div key={index} className={styles.singleFaq}>
-                  <AccordionButton
-                    item={item}
-                    index={index}
-                    activeAccordion={activeAccordion}
-                    setActiveAccordion={setActiveAccordion}
-                  />
-                  <div className={clsx(styles.faqContent, { [styles.show]: activeAccordion === index })}>
-                    <p>{item.answer}</p>
+              {accordionData.map((item, index) => {
+                const isActive = activeAccordion === index;
+                return (
+                  <div key={index} className={styles.faqContainer}>
+                    <button
+                      className={clsx(styles.faqButton, { [styles.active]: isActive })}
+                      type="button"
+                      onClick={() => handleAccordionClick(index)}
+                    >
+                      <span className={styles.faqButtonText}>{item.question}</span>
+                      <div className={styles.faqIcon}>
+                        <img src={plusIcon} alt="+" />
+                      </div>
+                    </button>
+                    <div className={clsx(styles.faqContent, { [styles.show]: isActive })}>
+                      <p className={clsx('description', styles.faqContentText, { [styles.show]: isActive })}>
+                        {item.answer}
+                      </p>
+                    </div>
                   </div>
-                </div>
-              ))}
+                );
+              })}
             </div>
             <Button href="#" onClick={(e) => e.preventDefault()} variant="primary">
               View More
